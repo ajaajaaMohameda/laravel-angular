669cb295e9de8cb751484b70c9ccb064975c781d
ajaajaaMohameda <ajaajaa.mohamed@gmail.com>
2021-06-02 11:13:09 +0000

    swagger

---
+++ chapter-04/project/app/Http/Controllers/ApiController.php
@@ -0,0 +1,29 @@
+
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Class ApiController
+     *
+     * @package App\Http\Controllers
+     *
+     * @OA\Swagger(
+     * @OA\PathItem(
+     *  path="/"
+     * )
+     *     basePath="",
+     *     host="localhost:8081",
+     *     schemes={"http"},
+     *     @OA\Info(
+     *         version="1.0",
+     *         title="Custom Bikes",
+     *         @OA\Contact(name="Developer Contact", url="https://www.example.com"),
+     *     )
+     * )
+     */
+}
