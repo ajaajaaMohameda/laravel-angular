da7f438c9069f90eff48c91f6b77a7e373d950d7
ajaajaaMohameda <ajaajaa.mohamed@gmail.com>
2021-06-04 14:44:22 +0000

    Forms validation

--- chapter-05/client/src/app/pages/auth/login/login.component.ts
+++ chapter-05/client/src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { AuthService } from '../_services/auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  error: any;
+  returnUrl: string;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'];
+  }
+
+  onSubmit(loginForm): void {
+    this.authService.onLogin(this.user).subscribe(response => {
+      this.router.navigate([this.returnUrl]);
+    }, (error) => {
+      this.error = error.error;
+    });
+
+    loginForm.reset();
   }
 
 }
--- chapter-05/client/src/app/pages/auth/register/register.component.ts
+++ chapter-05/client/src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User();
+  error: any;
+  registerForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.createForm();
+  }
 
   ngOnInit(): void {
   }
 
+  createForm() {
+    this.registerForm = this.fb.group({
+      name: [this.user.name, Validators.compose([Validators.required])],
+      email: [this.user.email, Validators.compose([Validators.required, Validators.email])],
+      password: [this.user.password, Validators.compose([Validators.required, Validators.minLength(6)])]
+    });
+  }
+
+  onSubmit(): void {
+    this.authService.onRegister(this.registerForm.value).subscribe(
+      response => {
+        this.router.navigate(['bikes']);
+      }, (response) => {
+        if(response.statuts === 422) {
+          Object.keys(response.error).map(err => {
+            this.error = `${response.error[err]}`
+          })
+        } else {
+          this.error = response.error;
+        }
+      }
+    )
+  }
 }
